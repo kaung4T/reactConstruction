6167ac3101e2ce23a68884187d01656926f0b49c
kaung4T <mrkaungminnkhant@gmail.com>
2025-01-30 21:49:21 +0630

    update

--- src/pages/Child.ts
+++ src/pages/Child.ts
@@ -2,11 +2,11 @@ import React from 'react'
 import Parent from './Parent'
 
 class Child extends Parent {
-    public name: string | null
+    // public name: string | null
 
     constructor (name: string | null = null) {
         super(name)
-        this.name = name
+        // this.name = name
     }
 
     fun () {
